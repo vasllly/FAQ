4ded735ec2d564f433114a043d2748cf4aefe553
Ganja Vasiliy <vasllly219@gmail.com>
2019-01-28 16:02:40 +0300

    Установлен Twig

--- config/app.php
+++ config/app.php
@@ -165,6 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
+        TwigBridge\ServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -223,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Twig' => TwigBridge\Facade\Twig::class,
 
     ],
 
