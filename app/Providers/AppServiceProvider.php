2c2395055d36f50ce178e7431c408357ee252720
Ganja Vasiliy <vasllly219@gmail.com>
2019-01-29 23:55:13 +0300

    Все шаблоны переделаны под twig

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+            $data = [
+                'locate' => str_replace('_', '-', app()->getLocale()),
+                'app_name' => config('app.name', 'Laravel'),
+                'auth' => Auth::check(),
+            ];
+
+            if ($data['auth']) {
+                $data['login'] = Auth::user()->login;
+            }
+
+            $view->with('data', $data);
+        });
     }
 
     /**
